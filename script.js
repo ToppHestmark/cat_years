e61c0137df32d9d9fcc75b1db1bee88734566472
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-06-25 11:02:17 +0200

    Cat years calculator

---
+++ script.js
@@ -0,0 +1,10 @@
+var myAge = 30;
+var earlyYears = 2;
+earlyYears *= 25;
+
+var laterYears = myAge - 2;
+laterYears *= 4;
+
+var myAgeInCatYears = earlyYears + laterYears;
+var myName = "Topp";
+console.log("My name is " + myName + " and my cat year is " + myAgeInCatYears + " years!")
